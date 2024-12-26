c0f6805a52839fb3c1a0aaa5466e0c6e26126a2e
Donal <donal.siagian@dentsu.com>
2024-12-26 22:12:25 +0700

    add login feature

---
+++ fe/components/auth/login-button.tsx
@@ -0,0 +1,37 @@
+"use client";
+import React from 'react'
+import { useRouter } from 'next/navigation';
+
+interface LoginButtonProps {
+    children: React.ReactNode;
+    mode?: 'modal' | 'redirect';
+    asChild?: boolean;
+}
+
+export const LoginButton = ({
+    children,
+    mode = 'redirect',
+    asChild
+}: LoginButtonProps) => {
+
+    const router = useRouter(); 
+
+    const onClickSpan = () => {
+        console.log("ROUTER REDIRECT")
+        router.push("/auth/login");
+    }
+
+    if (mode === 'modal') {
+        return (
+            <span onClick={() => { onClickSpan() }} className='cursor-pointer'>
+                TODO: Implement modal
+            </span>
+        )
+    }
+
+    return (
+        <span onClick={onClickSpan} className='cursor-pointer'>
+            {children}
+        </span>
+    )
+}
